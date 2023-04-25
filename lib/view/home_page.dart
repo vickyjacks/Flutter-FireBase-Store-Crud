cec2278ff4c416cdb293bc5f2f786d5fff2b2abf
Ankit Yadav <ankit.y@amsysinfo.com>
2023-04-25 13:07:22 +0530

    init

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -17,7 +17,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Row(
           children: [
-            Text("Fire Store"),
+            Text("Fire Store dsf cmjc cxweu cwui cw icec "),
             Spacer(),
             ElevatedButton(onPressed: (){
               Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AddStudent()));
